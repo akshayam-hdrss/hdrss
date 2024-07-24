d79191ca083fd42120eacb717ba7662c30c2af28
sebe <sebe2k04@gmail.com>
2024-07-24 11:01:27 +0530

    changes made

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 import { IoLogoWhatsapp } from "react-icons/io";
 import { FaFacebook } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
+import Marquee from "react-fast-marquee";
 
 export default function Page() {
   return (
@@ -37,9 +38,30 @@ export default function Page() {
               <div className="">
                 <h1 className="font-koulen text-4xl text-grey ">ABOUT US</h1>
                 <h5 className="pt-5 text-justify">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem esse molestias numquam, dolore provident id quas obcaecati, tenetur cum nisi distinctio iste ipsam voluptatum nostrum nam pariatur non eaque minus. lorem\
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde odio sit, recusandae facere laudantium cum quidem voluptas hic pariatur non sapiente dolore eaque accusamus possimus perferendis culpa rerum sunt velit! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore tenetur quae eius soluta eveniet! Inventore, repellendus, soluta a ut tempora minus veritatis reiciendis dolorem iste explicabo cum vel animi expedita.
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Dolorem esse molestias numquam, dolore provident id quas
+                  obcaecati, tenetur cum nisi distinctio iste ipsam voluptatum
+                  nostrum nam pariatur non eaque minus. lorem\ Lorem ipsum dolor
+                  sit amet consectetur adipisicing elit. Unde odio sit,
+                  recusandae facere laudantium cum quidem voluptas hic pariatur
+                  non sapiente dolore eaque accusamus possimus perferendis culpa
+                  rerum sunt velit! Lorem, ipsum dolor sit amet consectetur
+                  adipisicing elit. Labore tenetur quae eius soluta eveniet!
+                  Inventore, repellendus, soluta a ut tempora minus veritatis
+                  reiciendis dolorem iste explicabo cum vel animi expedita.
                 </h5>
+              </div>
+              <div className="py-5">
+                <h1 className="font-koulen text-4xl text-grey ">GALLERY</h1>
+                <div className="py-5">
+                  <section className=" overflow-hidden w-full text-center">
+                    <Marquee pauseOnClick pauseOnHover>
+                        <img src="/ac1.jpeg" alt="" className="h-[200px] mx-2" />
+                        <img src="/ac2.jpg" alt="" className="h-[200px] mx-2" />
+                        <img src="/ac3.jpg" alt="" className="h-[200px] mx-2" />
+                    </Marquee>
+                  </section>
+                </div>
               </div>
             </div>
           </div>
