2f760aa60e5319832dc8a5b1af8abe32ce4dece5
sebe <sebe2k04@gmail.com>
2024-07-24 11:06:27 +0530

    changes made

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,17 +4,18 @@ import { IoLogoWhatsapp } from "react-icons/io";
 import { FaFacebook } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
 import Marquee from "react-fast-marquee";
+import Footer from "@/components/ui/Footer";
 
 export default function Page() {
   return (
     <div>
+      <div className="fixed w-full top-0 z-[50]">
       <Header />
+      </div>
       <main>
         <div className="lg:grid grid-cols-5 relative z-[0]">
-          <div className="">
-            <Navbar />
-          </div>
-          <div className="col-span-4 ">
+        <Navbar />
+          <div className="col-span-4 pt-[60px]">
             <img src="/membermd.png" alt="" className="w-full" />
             <div className="flex justify-center w-full mt-[-100px] ">
               <div className="p-3 rounded-full shadow-2xl bg-white">
@@ -51,22 +52,25 @@ export default function Page() {
                   reiciendis dolorem iste explicabo cum vel animi expedita.
                 </h5>
               </div>
-              <div className="py-5">
-                <h1 className="font-koulen text-4xl text-grey ">GALLERY</h1>
+              
+            </div>
+            <div className="py-5">
+                <h1 className="font-koulen text-4xl text-grey px-10 lg:px-20">GALLERY</h1>
                 <div className="py-5">
                   <section className=" overflow-hidden w-full text-center">
                     <Marquee pauseOnClick pauseOnHover>
                         <img src="/ac1.jpeg" alt="" className="h-[200px] mx-2" />
                         <img src="/ac2.jpg" alt="" className="h-[200px] mx-2" />
                         <img src="/ac3.jpg" alt="" className="h-[200px] mx-2" />
+                        <img src="/ac4.jpg" alt="" className="h-[200px] mx-2" />
                     </Marquee>
                   </section>
                 </div>
               </div>
-            </div>
           </div>
         </div>
       </main>
+      <Footer/>
     </div>
   );
 }
