1d9297da1d139e82d99d8304130ca165cf7d4e91
sebe <sebe2k04@gmail.com>
2024-08-16 21:38:58 +0530

    3 games added

--- src/app/games/page.jsx
+++ src/app/games/page.jsx
@@ -49,6 +49,54 @@ export default function Page() {
                 </Link>
               </div>
             </div>
+            <div className="max-w-[500px] flex gap-6 mx-auto w-fit my-10 items-start justify-evenly bg-[#f8eae3] px-6 py-6 rounded-md cursor-pointer hover:scale-110 hover:drop-shadow-2xl transition-all ">
+              <img src="/chess.png" alt="chess"  className="md:w-[150px] w-[100px] my-auto" />
+              <div>
+                <h1 className="md:text-3xl text-xl font-bold mt-6 mb-1">Chess</h1>
+                <h2 className="md:text-lg mb-6">
+                  Test your Tactics, Be Vital and Make your Strategy
+                </h2>
+                <Link
+                  href={"https://www.chess.com/"}
+                  target="_blank"
+                  className="bg-kaavi text-white px-4 py-2 rounded-md "
+                >
+                  Play now
+                </Link>
+              </div>
+            </div>
+            <div className="max-w-[500px] flex gap-6 mx-auto w-fit my-10 items-start justify-evenly bg-[#f8eae3] px-6 py-6 rounded-md cursor-pointer hover:scale-110 hover:drop-shadow-2xl transition-all ">
+              <img src="/mario.jpeg" alt="mario"  className="md:w-[150px] w-[100px] my-auto" />
+              <div>
+                <h1 className="md:text-3xl text-xl font-bold mt-6 mb-1">Super Mario</h1>
+                <h2 className="md:text-lg mb-6">
+                Play the retro classic game Super Mario
+                </h2>
+                <Link
+                  href={"https://supermarioplay.com/"}
+                  target="_blank"
+                  className="bg-kaavi text-white px-4 py-2 rounded-md "
+                >
+                  Play now
+                </Link>
+              </div>
+            </div>
+            <div className="max-w-[500px] flex gap-6 mx-auto w-fit my-10 items-start justify-evenly bg-[#f8eae3] px-6 py-6 rounded-md cursor-pointer hover:scale-110 hover:drop-shadow-2xl transition-all ">
+              <img src="/words.png" alt="quiz"  className="md:w-[150px] w-[100px] my-auto" />
+              <div>
+                <h1 className="md:text-3xl text-xl font-bold mt-6 mb-1">Quiz</h1>
+                <h2 className="md:text-lg mb-6">
+                  Test your knowledge with Vocabulary
+                </h2>
+                <Link
+                  href={"https://word.tips/"}
+                  target="_blank"
+                  className="bg-kaavi text-white px-4 py-2 rounded-md "
+                >
+                  Play now
+                </Link>
+              </div>
+            </div>
            </div>
             
 
